b948a87d6dc2dee0795203366c037c485f47100a
ajay2512 <bhakarajay033@gmail.com>
2025-01-19 19:18:05 +0530

    authentication done and backend setup initialise

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
-
+import Header from "@/components/header";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "FinStack",
@@ -10,12 +12,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={``}
-      >
+      <body className={`${inter.className}`}>
+        <main className="min-h-screen">
+        <Header />
         {children}
+        </main>
+
+        <footer className="bg-blue-200 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made with love From AB</p>
+          </div>
+
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Divide } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <div  className="mt-40">
+
     <Button variant="destructive">
       this is the main content
+
     </Button>
+    </div>
+
   );
 }
